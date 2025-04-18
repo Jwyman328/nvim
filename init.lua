3bf7b8ab7683d41c5eba9c84fdd622df0a8a932f
Joseph Wyman <jwymandev@gmail.com>
2025-04-18 02:15:24 -0400

    add keymap for telescope git status

--- init.lua
+++ init.lua
@@ -664,18 +664,20 @@ local function telescope_live_grep_open_files()
 end
 
 vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
-vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', find_files_custom_display, {
   desc = '[S]earch [F]iles',
 })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>ss', ':Telescope git_status<cr>', { desc = '[S]earch git [S]tatus' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', livegrep_custom_display, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>sp', ':Telescope harpoon marks<CR>', { desc = 'Harpoon Marks' })
+
 -- harpoon
 vim.keymap.set('n', '<leader>pm', require('harpoon.mark').add_file, { desc = 'harpoon mark' })
 vim.keymap.set('n', '<leader>pn', require('harpoon.ui').nav_next, { desc = 'harpoon next' })
